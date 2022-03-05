15b59d390a8925980ec9ab595a6cb64b9674db57
Josh <adesanyajoshua@ymail.com>
2022-03-05 07:30:25 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,21 @@ export default function Footer() {
   return (
     <div>
       <div className="footer">
-        <div>
-          <div className="footer-item">Logo</div>
+        <div className="footer1">
+          <div className="footer-item">
+            <img src="/images/logo.jpg" alt="logo" style={{ width: "100px" }} />
+          </div>
           <div className="footer-item">About Us</div>
           <div className="footer-item">Team</div>
         </div>
 
-        <div>
+        <div className="footer2">
           <div className="footer-item">Privacy Policy</div>
           <div className="footer-item">Terms and Condition</div>
           <div className="footer-item">Tokenomics</div>
         </div>
 
-        <div>
+        <div className="footer3">
           <div className="footer-item">Email Address</div>
           <div className="footer-item">Phone Number</div>
           <div className="footer-item">Social Links</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
+import Web3Modal from "web3modal";
+import Web3 from "web3";
+import { providers, Contract } from "ethers";
 
 export default function Header() {
   const [show, setShow] = useState(false);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [address, setAddress] = useState("");
   const changeBar = () => {
     setShow(!show);
   };
+  const web3ModalRef = useRef();
+  let provider;
+  let web3Modal;
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+
+    web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions: {}, // required
+    });
+
+    provider = await web3Modal.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    // Get list of accounts of the connected wallet
+    const web3 = new Web3(provider);
+    // Get list of accounts of the connected wallet
+    const accounts = await web3.eth.getAccounts();
+    setAddress(accounts[0]);
+
+    // If user is not connected to the Mainnet network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 1) {
+      window.alert("Change the network to mainnet");
+      throw new Error("Change network to mainnet");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      console.log(signer);
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      const details = await getProviderOrSigner();
+      setWalletConnected(true);
+
+      // Subscribe to accounts change
+      provider.on("accountsChanged", async (accounts) => {
+        console.log("acc", accounts);
+        await getProviderOrSigner();
+      });
+
+      // Subscribe to chainId change
+      provider.on("chainChanged", async (chainId) => {
+        console.log("chain", chainId);
+        await getProviderOrSigner();
+      });
+
+      // Subscribe to provider connection
+      provider.on("connect", (info) => {
+        console.log("inf", info);
+      });
+
+      // Subscribe to provider disconnection
+      provider.on("disconnect", (error) => {
+        console.log("dis", error);
+        setAddress("");
+        setWalletConnected(false);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    setAddress("");
+  };
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "mainnet",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
     <div className="header">
       <div style={{ display: "flex" }}>
@@ -61,11 +156,21 @@ export default function Header() {
           className={(navData) => (navData.isActive ? "active" : "normal")}
           to="/dapp"
         >
-          <div className="nav-item">Dapp</div>
+          <div className="nav-item" style={{ marginBottom: "20px" }}>
+            Dapp
+          </div>
         </NavLink>
       </div>
 
-      <button className="wallet">Connect Wallet</button>
+      {walletConnected && address.length > 0 ? (
+        <button onClick={disconnectWallet} className="wallet">
+          {address.slice(0, 5) + "....." + address.slice(-5, -1)}
+        </button>
+      ) : (
+        <button onClick={connectWallet} className="wallet">
+          Connect Wallet
+        </button>
+      )}
     </div>
   );
 }
